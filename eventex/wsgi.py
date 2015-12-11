7d5dea8fc3b5e5ebf2cd7d5a61cff1ba50be55c4
Hiarison Gigante <gigantedesousa@gmail.com>
2015-12-11 02:26:55 -0300

    Import project

---
+++ eventex/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for eventex project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+from dj_static import Cling
+from django.core.wsgi import  get_wsgi_application
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
+
+application = Cling(get_wsgi_application())
