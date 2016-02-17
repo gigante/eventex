af89a8bbb9995c37d33ba505bbeaa2a5f8856cfd
Hiarison Gigante <gigantedesousa@gmail.com>
2016-02-17 13:44:30 -0300

    Padroniza nomes

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -15,3 +15,8 @@ class SubscriptionForm(forms.Form):
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
     email = forms.EmailField(label='Email')
     phone = forms.CharField(label='Telefone')
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        words = [w.capitalize() for w in name.split()]
+        return ' '.join(words)
