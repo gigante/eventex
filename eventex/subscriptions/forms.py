609f9092d1eb950568d4e1e4e63b03d74b6ed0ab
Hiarison Gigante <gigantedesousa@gmail.com>
2016-02-18 16:14:08 -0300

    Simplifica o ModelForm

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,16 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from eventex.subscriptions.models import Subscription
+from eventex.subscriptions.validators import validate_cpf
 
 
-def validate_cpf(value):
-    if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números', 'digits')
-
-    if len(value) != 11:
-        raise ValidationError('CPF deve ter 11 números', 'length')
-
-
-class SubscriptionForm(forms.Form):
+class SubscriptionFormOld(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
     email = forms.EmailField(label='Email', required=False)
@@ -22,6 +16,25 @@ class SubscriptionForm(forms.Form):
         return ' '.join(words)
 
     def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone')
+
+        return self.cleaned_data
+
+
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        words = [w.capitalize() for w in name.split()]
+        return ' '.join(words)
+
+    def clean(self):
+        self.cleaned_data = super().clean()
+
         if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
             raise ValidationError('Informe seu e-mail ou telefone')
 
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -26,7 +26,7 @@ def create(request):
         return render(request, 'subscriptions/subscription_form.html',
                       {'form': form})
 
-    subscription = Subscription.objects.create(**form.cleaned_data)
+    subscription = form.save()
 
     # Send Email
     template_name = 'subscriptions/subscription_email.txt'
