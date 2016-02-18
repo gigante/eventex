37c096af159b6893c993c3bb911ec52e5ea4806a
Hiarison Gigante <gigantedesousa@gmail.com>
2016-02-18 16:19:39 -0300

    retirado SubscriptionFormOld

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,25 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from eventex.subscriptions.models import Subscription
-from eventex.subscriptions.validators import validate_cpf
-
-
-class SubscriptionFormOld(forms.Form):
-    name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email', required=False)
-    phone = forms.CharField(label='Telefone', required=False)
-
-    def clean_name(self):
-        name = self.cleaned_data['name']
-        words = [w.capitalize() for w in name.split()]
-        return ' '.join(words)
-
-    def clean(self):
-        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
-            raise ValidationError('Informe seu e-mail ou telefone')
-
-        return self.cleaned_data
 
 
 class SubscriptionForm(forms.ModelForm):
