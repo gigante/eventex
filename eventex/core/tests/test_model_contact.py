06c7e5ff71b21214b433290fff952ef329f4952a
Hiarison Gigante <gigantedesousa@gmail.com>
2016-02-24 18:07:39 -0300

    Managers

--- eventex/core/tests/test_model_contact.py
+++ eventex/core/tests/test_model_contact.py
@@ -31,3 +31,25 @@ class ContentModelTest(TestCase):
     def test_str(self):
         contact = Contact(speaker=self.speaker, kind=Contact.EMAIL, value='henrique@bastos.net')
         self.assertEqual('henrique@bastos.net', str(contact))
+
+
+class ContactManagerTest(TestCase):
+    def setUp(self):
+        s = Speaker.objects.create(
+            name='Henrique Bastos',
+            slug='henrique-bastos',
+            photo='http://hbn.link/hb-pic'
+        )
+
+        s.contact_set.create(kind=Contact.EMAIL, value='henrique@bastos.net')
+        s.contact_set.create(kind=Contact.PHONE, value='21-996186180')
+
+    def test_emails(self):
+        qs = Contact.objects.emails()
+        expected = ['henrique@bastos.net']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
+
+    def test_phones(self):
+        qs = Contact.objects.phones()
+        expected = ['21-996186180']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
